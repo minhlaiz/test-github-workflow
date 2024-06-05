f8211a91d21518a66984c04c4a2b1019e57deec0
Minh Lai <minh.lai@tokyotechies.com>
2024-06-05 17:20:20 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ function App() {
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+
+          asdfadsf
         </a>
       </div>
       <h1>Vite + React</h1>
