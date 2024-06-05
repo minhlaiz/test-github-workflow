4c571fcff1206b6dca34a0d2a44040fe62553433
Minh Lai <minh.lai@tokyotechies.com>
2024-06-05 16:40:21 +0900

    add change

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+
+
+      change
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
